e989d2b7bb70dc93c220fbe9edae9dfc4077bbc9
Haris Wu <bymark256@gmail.com>
2023-10-31 04:03:26 +0800

    scripts: fix build cache time error
    Change-Id: Ieb97939757b2cc01a0ab6a28f8d2beb44fdf8917
    Signed-off-by: Haris Wu <bymark256@gmail.com>

--- info.sh
+++ info.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-d=$(date +%Y%m%d)
+build_cache=$(grep -r 'ro.lineage.version=' ~/lineage/out/target/product/oxygen/system/build.prop | cut -d '=' -f 2 |  cut -b 6-13)
 oldd=$(grep filename los-20.json | cut -d '-' -f 3)
-md5=$(md5sum ../lineage/out/target/product/oxygen/lineage-20.0-"${d}"-UNOFFICIAL-oxygen.zip | cut -d ' ' -f 1)
+md5=$(md5sum ../lineage/out/target/product/oxygen/lineage-20.0-"${build_cache}"-UNOFFICIAL-oxygen.zip | cut -d ' ' -f 1)
 oldmd5=$(grep '"id"' los-20.json | cut -d':' -f 2)
 utc=$(grep ro.build.date.utc ../lineage/out/target/product/oxygen/system/build.prop | cut -d '=' -f 2)
 oldutc=$(grep datetime los-20.json | cut -d ':' -f 2)
-size=$(wc -c ../lineage/out/target/product/oxygen/lineage-20.0-"${d}"-UNOFFICIAL-oxygen.zip | cut -d ' ' -f 1)
+size=$(wc -c ../lineage/out/target/product/oxygen/lineage-20.0-"${build_cache}"-UNOFFICIAL-oxygen.zip | cut -d ' ' -f 1)
 oldsize=$(grep size los-20.json | cut -d ':' -f 2)
 oldurl=$(grep url los-20.json | cut -d ' ' -f 8)
-
-# This is where the magic happens
 
 sed -i "s!${oldmd5}! \"${md5}\",!g" los-20.json
 sed -i "s!${oldutc}! \"${utc}\",!g" los-20.json
 sed -i "s!${oldsize}! \"${size}\",!g" los-20.json
-sed -i "s!${oldd}!${d}!" los-20.json
-echo Enter the new Download URL
+sed -i "s!${oldd}!${build_cache}!" los-20.json
+echo New Download URL
 read -r url
 sed -i "s!${oldurl}!\"${url}\",!g" los-20.json
