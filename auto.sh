eb22218eb085598314dc29fb45fdf16f7ead4c61
Haris Wu <bymark256@gmail.com>
2023-11-03 00:06:14 +0800

    scripts: remove input action
    Change-Id: I13e31368118107ec480baff25a932d4a1837c1ca

--- auto.sh
+++ auto.sh
@@ -1,5 +1,6 @@
 build_cache=$(grep -r 'ro.lineage.version=' ~/lineage/out/target/product/oxygen/system/build.prop | cut -d '=' -f 2 |  cut -b 6-13)
 oldd=$(grep filename los-20.json | cut -d '-' -f 3)
+echo "Wait ..."
 md5=$(md5sum ../lineage/out/target/product/oxygen/lineage-20.0-"${build_cache}"-UNOFFICIAL-oxygen.zip | cut -d ' ' -f 1)
 oldmd5=$(grep '"id"' los-20.json | cut -d':' -f 2)
 utc=$(grep ro.build.date.utc ../lineage/out/target/product/oxygen/system/build.prop | cut -d '=' -f 2)
@@ -12,8 +13,7 @@ sed -i "s!${oldmd5}! \"${md5}\",!g" los-20.json
 sed -i "s!${oldutc}! \"${utc}\",!g" los-20.json
 sed -i "s!${oldsize}! \"${size}\",!g" los-20.json
 sed -i "s!${oldd}!${build_cache}!" los-20.json
-echo New Download URL
-read -r url
+url="https://sourceforge.net/projects/max-2-pixelexperience-ota/files/LineageOS/lineage-20.0-"$build_cache"-UNOFFICIAL-oxygen.zip/download"
 sed -i "s!${oldurl}!\"${url}\",!g" los-20.json
 
 # Auto submit
