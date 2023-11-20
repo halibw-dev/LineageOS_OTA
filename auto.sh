884721426ec42cdc916c6e5b8eba3e018a014594
Haris Wu <bymark256@gmail.com>
2023-11-20 18:10:21 +0800

    lineage-21
    Change-Id: I86c7ee5cfbc419c8fe452014944f300a8de9cff3

--- auto.sh
+++ auto.sh
@@ -1,23 +1,23 @@
-build_cache=$(grep -r 'ro.lineage.version=' ~/lineage/out/target/product/oxygen/system/build.prop | cut -d '=' -f 2 |  cut -b 6-13)
-oldd=$(grep filename los-20.json | cut -d '-' -f 3)
+build_cache=$(grep -r 'ro.lineage.version=' /mnt/m2/lineage/out/target/product/oxygen/system/build.prop | cut -d '=' -f 2 | cut -b 6-13)
+oldd=$(grep filename los-21.json | cut -d '-' -f 3)
 echo "Wait ..."
-md5=$(md5sum ../lineage/out/target/product/oxygen/lineage-20.0-"${build_cache}"-UNOFFICIAL-oxygen.zip | cut -d ' ' -f 1)
-oldmd5=$(grep '"id"' los-20.json | cut -d':' -f 2)
-utc=$(grep ro.build.date.utc ../lineage/out/target/product/oxygen/system/build.prop | cut -d '=' -f 2)
-oldutc=$(grep datetime los-20.json | cut -d ':' -f 2)
-size=$(wc -c ../lineage/out/target/product/oxygen/lineage-20.0-"${build_cache}"-UNOFFICIAL-oxygen.zip | cut -d ' ' -f 1)
-oldsize=$(grep size los-20.json | cut -d ':' -f 2)
-oldurl=$(grep url los-20.json | cut -d ' ' -f 8)
+md5=$(md5sum /mnt/m2/lineage/out/target/product/oxygen/lineage-21.0-"${build_cache}"-UNOFFICIAL-oxygen.zip | cut -d ' ' -f 1)
+oldmd5=$(grep '"id"' los-21.json | cut -d':' -f 2)
+utc=$(grep ro.build.date.utc /mnt/m2/lineage/out/target/product/oxygen/system/build.prop | cut -d '=' -f 2)
+oldutc=$(grep datetime los-21.json | cut -d ':' -f 2)
+size=$(wc -c /mnt/m2/lineage/out/target/product/oxygen/lineage-21.0-"${build_cache}"-UNOFFICIAL-oxygen.zip | cut -d ' ' -f 1)
+oldsize=$(grep size los-21.json | cut -d ':' -f 2)
+oldurl=$(grep url los-21.json | cut -d ' ' -f 8)
 
-sed -i "s!${oldmd5}! \"${md5}\",!g" los-20.json
-sed -i "s!${oldutc}! \"${utc}\",!g" los-20.json
-sed -i "s!${oldsize}! \"${size}\",!g" los-20.json
-sed -i "s!${oldd}!${build_cache}!" los-20.json
-url="https://sourceforge.net/projects/max-2-pixelexperience-ota/files/LineageOS/lineage-20.0-"$build_cache"-UNOFFICIAL-oxygen.zip/download"
-sed -i "s!${oldurl}!\"${url}\",!g" los-20.json
+sed -i "s!${oldmd5}! \"${md5}\",!g" los-21.json
+sed -i "s!${oldutc}! \"${utc}\",!g" los-21.json
+sed -i "s!${oldsize}! \"${size}\",!g" los-21.json
+sed -i "s!${oldd}!${build_cache}!" los-21.json
+url="https://sourceforge.net/projects/max-2-pixelexperience-ota/files/LineageOS/lineage-21.0-"$build_cache"-UNOFFICIAL-oxygen.zip/download"
+sed -i "s!${oldurl}!\"${url}\",!g" los-21.json
 
 # Auto submit
 date=$(date +%Y%m%d)
-message="Update los-20.json "\`$date\`""
+message="Update los-21.json "\`$date\`""
 git add .
 git commit -m "$message"
